7b802cccae5bc5a2a8c51acd8fb297b6ec3f4000
Eli Collins <eli.collins@gmail.com>
2013-05-25 16:32:23 -0700

    Add power of two choices demo.

---
+++ power2.go
@@ -0,0 +1,52 @@
+package main
+
+import(
+  "fmt"
+  "math"
+  "math/rand"
+  "time"
+)
+
+const(N=1000000)
+
+func noChoice() {
+    var bins[N]int
+    var max int
+    var EXPECTED_MAX = math.Log(N) / math.Log(math.Log(N))
+    for i := 0; i < N; i++ {
+      bins[rand.Intn(N)]++
+    }
+    for i := 0; i < N; i++ {
+      if bins[i] > max {
+        max = bins[i];
+      }
+    }
+    fmt.Println(max, EXPECTED_MAX)
+}
+
+func twoChoice() {
+    var bins[N]int
+    var max int
+    var EXPECTED_MAX = math.Log(math.Log(N)) / math.Log(2) + 1
+    for i := 0; i < N; i++ {
+      x := rand.Intn(N)
+      y := rand.Intn(N)
+      if bins[x] > bins[y] {
+        bins[y]++;
+      } else {
+        bins[x]++;
+      }
+    }
+    for i := 0; i < N; i++ {
+      if bins[i] > max {
+        max = bins[i];
+      }
+    }
+    fmt.Println(max, EXPECTED_MAX)
+}
+
+func main() {
+    rand.Seed(time.Now().UnixNano())
+    noChoice()
+    twoChoice()
+}
